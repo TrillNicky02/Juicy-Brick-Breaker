94789d036297fcd39a257271b1b85d5f057a2ebc
TrillNicky02 <nivictor@iu.edu>
2023-10-02 07:52:14 -0400

    completed project 3 part 1

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -9,9 +9,30 @@ var powerup_prob = 0.1
 func _ready():
 	randomize()
 	position = new_position
+	
+	if score >= 100:
+		var Brick = load("res://Brick/Brick1.tscn")
+		
+	elif score >= 90:
+		var Brick = load("res://Brick/Brick2.tscn")
+	elif score >= 80:
+		var Brick = load("res://Brick/Brick3.tscn")
+	elif score >= 70:
+		var Brick = load("res://Brick/Brick4.tscn")
+	elif score >= 60:
+		var Brick = load("res://Brick/Brick5.tscn")
+	elif score >= 50:
+		var Brick = load("res://Brick/Brick6.tscn")
+	elif score >= 40:
+		var Brick = load("res://Brick/Brick7.tscn")
+	elif score >= 30:
+		var Brick = load("res://Brick/Brick8.tscn")
+	else:
+		var Brick = load("res://Brick/Brick.tscn")
+		
 
 func _physics_process(_delta):
-	if dying:
+	if dying and not $Confetti.emitting:
 		queue_free()
 
 func hit(_ball):
@@ -20,6 +41,7 @@ func hit(_ball):
 func die():
 	dying = true
 	collision_layer = 0
+	$Confetti.emitting = true
 	$ColorRect.hide()
 	Global.update_score(score)
 	if not Global.feverish:
